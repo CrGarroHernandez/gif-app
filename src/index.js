b16a6e549305aa07f2f502be071d9a45641aa9af
CrGarroHernandez <c.garro.hernandez@gmail.com>
2022-03-24 15:55:22 -0300

    Linkeando componente principal respectivo y borrado de link sin utilizar

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import GifExpertApp from './GifExpertApp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GifExpertApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
