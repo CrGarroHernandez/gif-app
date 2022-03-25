4bd52d29fb4c03dbda12f72b2dc6c4b2c6379f37
CrGarroHernandez <c.garro.hernandez@gmail.com>
2022-03-24 23:36:08 -0300

    Ordenar categoría, creación de helper para petición a API además de dinámica

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,31 +1,14 @@
 import React, { useState, useEffect } from 'react'
+import { getGifs } from '../helpers/getGifs';
 import { GridItem } from './GridItem';
 
 export const GifGrid = ({category}) => {
     const [images, setImages] = useState([]);
 
     useEffect(() => {
-        getGifs();
-    }, []);
-
-    const getGifs = async () => {
-        const q = 'cr7';
-        const limit = '10';
-        const url = `https://api.giphy.com/v1/gifs/search?q=${q}&limit=${limit}&apikey=Oom6bcCC1Rf3FLTwhOTUyU2xcRpusGdx`;
-        const resp = await fetch(url);
-        const {data} = await resp.json();
-        
-        const gifs = data.map(img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-
-        console.log(gifs);
-        setImages(gifs);
-    }
+        getGifs(category)
+            .then(setImages);
+    }, [category]);
 
     return (
         <>
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async (category) => {
+    const limit = '10';
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=${limit}&apikey=Oom6bcCC1Rf3FLTwhOTUyU2xcRpusGdx`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+    
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifs;
+}
